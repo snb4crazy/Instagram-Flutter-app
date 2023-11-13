eafafee560fee1707750aca8291b3231ffb507a3
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-12 20:17:45 -0500

    Main: init Firebase app on the start

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/responsive_layout.dart';
 import 'package:instagram/responsive/web_screen_layout.dart';
 import 'package:instagram/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
