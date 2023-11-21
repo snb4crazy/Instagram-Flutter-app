92fb1bb178e2c129ef5e42084deb74dccedb006f
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-20 22:08:59 -0500

    LogIn Screen: add email field & dispose controllers

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import '../utils/colors.dart';
+import 'package:instagram/utils/colors.dart';
+import 'package:instagram/utils/dimensions.dart';
+import 'package:instagram/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -11,12 +13,21 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
       child: Container(
-        padding: MediaQuery.of(context).size.width > 640
+        padding: MediaQuery.of(context).size.width > webScreenSize
             ? EdgeInsets.symmetric(
                 horizontal: MediaQuery.of(context).size.width / 3)
             : const EdgeInsets.symmetric(horizontal: 32),
@@ -24,6 +35,7 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Flexible(child: Container(), flex: 2),
             SvgPicture.asset(
               'assets/ic_instagram.svg',
               color: primaryColor,
@@ -31,7 +43,12 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             const SizedBox(
               height: 64,
-            )
+            ),
+            TextFieldInput(
+              hintText: 'Enter your email',
+              textInputType: TextInputType.emailAddress,
+              textEditingController: _emailController,
+            ),
             //auth input email & password
             //buttons: login & signup
           ],
