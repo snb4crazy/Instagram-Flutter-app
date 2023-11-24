21e126d7efa83d686e056e90858faab645c3cbd7
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-24 18:45:54 -0500

    Signup screen: avatar image upload

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,12 +1,11 @@
 import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram/resources/storage_methods.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  // Signing Up User
 
   Future<String> signUpUser({
     required String email,
@@ -26,6 +25,10 @@ class AuthMethods {
           password: password,
         );
 
+        String photoUrl =
+            await StorageMethods().uploadImageToStorage('profile', file, false);
+        //if use .add instead of .set - name & uid will be different, with set - match
+        //await _firestore.collection('users').add({
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'user': username,
           'uid': cred.user!.uid,
@@ -33,6 +36,7 @@ class AuthMethods {
           'bio': bio,
           'followers': [],
           'following': [],
+          'photoUrl': photoUrl,
         });
 
         res = "success";
