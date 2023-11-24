4047c5d0a72c2e5bc0f6715d3975752afb7db2c9
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-24 01:58:18 -0500

    Firebase storage upload image method

---
+++ lib/resources/storage_methods.dart
@@ -0,0 +1,25 @@
+import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  String downloadUrl = '';
+
+  Future<String> uploadImageToStorage(
+      String childName, Uint8List file, bool isPost) async {
+    Reference ref =
+        _storage.ref().child(childName).child(_auth.currentUser!.uid);
+    UploadTask uploadTask = ref.putData(file);
+
+    try {
+      TaskSnapshot snapshot = await ref.putData(file);
+      String downloadUrl = await ref.getDownloadURL();
+    } catch (e) {
+      print(e);
+    }
+    return downloadUrl;
+  }
+}
