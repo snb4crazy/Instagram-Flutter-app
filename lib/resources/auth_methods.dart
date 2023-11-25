1a3aacfe5616dc5a1f495201e5e10566b5a464e0
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-25 15:15:47 -0500

    Refactoring: auth methods with user model

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:instagram/resources/storage_methods.dart';
+import 'package:instagram/models/user.dart' as model;
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -10,7 +11,7 @@ class AuthMethods {
   Future<String> signUpUser({
     required String email,
     required String password,
-    required String username,
+    required String name,
     required String bio,
     required Uint8List file,
   }) async {
@@ -18,7 +19,7 @@ class AuthMethods {
     try {
       if (email.isNotEmpty ||
           password.isNotEmpty ||
-          username.isNotEmpty ||
+          name.isNotEmpty ||
           bio.isNotEmpty) {
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
           email: email,
@@ -27,17 +28,21 @@ class AuthMethods {
 
         String photoUrl =
             await StorageMethods().uploadImageToStorage('profile', file, false);
+
+        model.User user = model.User(
+          name: name,
+          uid: cred.user!.uid,
+          email: email,
+          bio: bio,
+          followers: [],
+          following: [],
+          photoUrl: photoUrl,
+        );
         //if use .add instead of .set - name & uid will be different, with set - match
         //await _firestore.collection('users').add({
-        await _firestore.collection('users').doc(cred.user!.uid).set({
-          'user': username,
-          'uid': cred.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl,
-        });
+        await _firestore.collection('users').doc(cred.user!.uid).set(
+              user.toJson(),
+            );
         res = 'success';
       } else {
         res = 'Please enter all the fields';
