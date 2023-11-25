0630cded6ec3c7b4f694e5437d7f40bbca38f8c9
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-25 18:40:44 -0500

    Add Post Screen: refactoring

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,7 +1,24 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 
-class AddPostScreen extends StatelessWidget {
+class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+  bool isLoading = false;
+  final TextEditingController _descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _descriptionController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
