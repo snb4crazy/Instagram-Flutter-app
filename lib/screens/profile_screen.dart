d38ff051326baf029e096c7231681229285e5ea2
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-25 16:46:01 -0500

    Stub widget Profile Screen

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final String uid;
+  const ProfileScreen({Key? key, required this.uid}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
