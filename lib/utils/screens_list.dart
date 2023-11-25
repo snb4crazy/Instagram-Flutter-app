314b64c4109cccb1defd6eaace63273680e4ab83
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-25 16:47:13 -0500

    Refactoring Screens items list

---
+++ lib/utils/screens_list.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram/screens/add_post_screen.dart';
+import 'package:instagram/screens/feed_screen.dart';
+import 'package:instagram/screens/profile_screen.dart';
+import 'package:instagram/screens/search_screen.dart';
+
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
+];
