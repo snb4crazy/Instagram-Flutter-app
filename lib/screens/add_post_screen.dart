ea542bfe9f5250276bea00a663c868d008869c7d
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-28 22:27:38 -0500

    AddPostScreen: post image method

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
+import 'package:instagram/utils/util_functions.dart';
+import 'package:instagram/resources/firestore_access.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -24,6 +25,47 @@ class _AddPostScreenState extends State<AddPostScreen> {
     setState(() {
       _file = null;
     });
+  }
+
+  void postImage(String uid, String username, String profImage) async {
+    setState(() {
+      isLoading = true;
+    });
+    // start the loading
+    try {
+      // upload to storage and db
+      String res = await FireStoreAccess().uploadPost(
+        _descriptionController.text,
+        _file!,
+        uid,
+        username,
+        profImage,
+      );
+      if (res == "success") {
+        setState(() {
+          isLoading = false;
+        });
+        if (context.mounted) {
+          showSnackBar(
+            context,
+            'Posted!',
+          );
+        }
+        clearImage();
+      } else {
+        if (context.mounted) {
+          showSnackBar(context, res);
+        }
+      }
+    } catch (err) {
+      setState(() {
+        isLoading = false;
+      });
+      showSnackBar(
+        context,
+        err.toString(),
+      );
+    }
   }
 
   @override
