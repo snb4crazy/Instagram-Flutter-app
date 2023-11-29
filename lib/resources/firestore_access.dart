32817bd1665c0f175fcc1189889b37c81411c6d5
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-11-28 21:46:16 -0500

    Firestore Access: upload Post method

---
+++ lib/resources/firestore_access.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:instagram/models/post.dart';
+import 'package:instagram/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FireStoreAccess {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> uploadPost(String description, Uint8List file, String uid,
+      String name, String profImage) async {
+    // asking uid here because we dont want to make extra calls to firebase auth when we can just get from our state management
+    String res = "Some error occurred";
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+      String postId = const Uuid().v1(); // creates unique id based on time
+      Post post = Post(
+        description: description,
+        uid: uid,
+        name: name,
+        likes: [],
+        postId: postId,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profImage: profImage,
+      );
+      _firestore.collection('posts').doc(postId).set(post.toJson());
+      res = "success";
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
