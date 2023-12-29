81a7236855009631b098f5f032271bc7ba12165e
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-12-28 19:13:42 -0500

    Fix Firebase storage methods post upload

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -1,25 +1,27 @@
 import 'dart:typed_data';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 class StorageMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseStorage _storage = FirebaseStorage.instance;
-  String downloadUrl = '';
 
+  // adding image to firebase storage
   Future<String> uploadImageToStorage(
       String childName, Uint8List file, bool isPost) async {
+    // creating location to our firebase storage
     Reference ref =
         _storage.ref().child(childName).child(_auth.currentUser!.uid);
+    if (isPost) {
+      String id = const Uuid().v1();
+      ref = ref.child(id);
+    }
+    // putting in uint8list format -> Upload task like a future but not future
     UploadTask uploadTask = ref.putData(file);
 
-    try {
-      TaskSnapshot snapshot = await ref.putData(file);
-      String downloadUrl = await ref.getDownloadURL();
-    } catch (e) {
-      print(e);
-    }
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
     return downloadUrl;
   }
 }
